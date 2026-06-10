4d5a6dede09ed0daf4ea261f814a3efcafebfb39
munnep <patrick_118@msn.com>
2026-06-10 13:52:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+# module "test" {
+#   source = "git::https://github.com/munnep-practice/test"
+# }
+
 module "test" {
-  source = "git::https://github.com/munnep-practice/test"
+  source = "git::ssh://munnep@github.com/munnep-practice/test"
 }
