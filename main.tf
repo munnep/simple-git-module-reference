06c8fe442c9e701c4345132c4c687f4f103e0c1e
munnep <patrick_118@msn.com>
2026-06-10 14:21:48 +0200

    Add 'test2' module and remove 'test' module
    Replaced commented 'test' module with 'test2' module.

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@
 #   source = "git::https://github.com/munnep-practice/test"
 # }
 
-# module "test" {
-#   source = "git:github.com/munnep/null_resource"
-# }
+module "test2" {
+  source = "git::github.com/munnep-practice/test"
+}
+
 
 # module "test" {
 #   source = "git::ssh://git@github.com/munnep/null_resource.git"
