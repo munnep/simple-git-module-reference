8638d14ca50dfb7cfdd98bc689aa8df7a7958f7f
munnep <patrick_118@msn.com>
2026-06-10 14:05:07 +0200

    Update module source to use SSH URL

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@
 #   source = "git::https://github.com/munnep-practice/test"
 # }
 
+# module "test" {
+#   source = "git:github.com/munnep/null_resource"
+# }
+
 module "test" {
-  source = "git:github.com/munnep/null_resource"
+  source = "git::ssh://git@github.com/munnep/null_resource.git"
 }
